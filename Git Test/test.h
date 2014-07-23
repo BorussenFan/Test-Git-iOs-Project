7728790d1315071287d304b5f0d85aae6ff35aa2
Thomas Andrien <thomasandrien@andrien.speedport.ip>
2014-07-23 21:19:54 +0200

    added a test class

---
+++ Git Test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  Git Test
+//
+//  Created by Thomas Andrien on 23.07.14.
+//  Copyright (c) 2014 Thomas Andrien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ Git Test/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  Git Test
+//
+//  Created by Thomas Andrien on 23.07.14.
+//  Copyright (c) 2014 Thomas Andrien. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
